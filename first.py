30192ae727feae2c3bde34b147ba0f0dcb55e472
Atharv sapale <atharvsapale90@gmail.com>
2025-11-28 12:25:27 +0530

    This is third commit which contains all data of previous commits also.

--- first.py
+++ first.py
@@ -5,3 +5,7 @@ print("This is a test file for git status command.")
 #this line is added after first commit 
 print("This line is added after the first commit.")
 #this part is second commit part for trial
+
+#this line is added for third commit
+print("This line is added for the third commit.")
+#this part is third commit part for trial
