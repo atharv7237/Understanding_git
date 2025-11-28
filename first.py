b408a8f472258bb99f72c77b29ade797b38d0e34
Atharv sapale <atharvsapale90@gmail.com>
2025-11-28 12:19:26 +0530

    This is my second commit this will not be displayed in first commit.

--- first.py
+++ first.py
@@ -1,3 +1,7 @@
 #this file is created for testing git status command
 print("This is a test file for git status command.")
 #this upper part is the first commit of my new repoo
+
+#this line is added after first commit 
+print("This line is added after the first commit.")
+#this part is second commit part for trial
